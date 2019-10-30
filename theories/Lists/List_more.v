2766ecbfc8bb6a4cb795d88464437942131ddb22
Olivier Laurent <olivier.laurent@ens-lyon.fr>
2019-10-30 12:54:16 +0100

    integration of Exists_or and Forall_and

--- theories/Lists/List_more.v
+++ theories/Lists/List_more.v
@@ -1,17 +1,10 @@
-(* List_more Library *)
-
-(** * Add-ons for List library
-Usefull properties apparently missing in the List library. *)
-
 Require Export List.
 Require Import PeanoNat.
 Require Import Lt Le Plus Max.
 Require Import Lia.
 
 
-
-(** ** Simplification in lists *)
-
+(* not for stdlib *)
 Ltac list_simpl :=
   repeat (
     repeat simpl ;
@@ -42,14 +35,11 @@ Ltac list_simpl_hyps :=
   end.
 Ltac list_simpl_all := list_simpl_hyps ; list_simpl.
 
-
-(** ** Removal of [cons] constructions *)
-
+(* not for stdlib *)
 Lemma cons_is_app {A} : forall (x:A) l, x :: l = (x :: nil) ++ l.
 Proof.
 reflexivity.
 Qed.
-
 Ltac cons2app :=
   repeat
   match goal with
@@ -84,9 +74,7 @@ Ltac cons2app_hyps :=
   end.
 Ltac cons2app_all := cons2app_hyps ; cons2app.
 
-
-(** ** Inversions of list equalities *)
-
+(* not for stdlib *)
 Ltac unit_vs_elt_inv H :=
   match type of H with
   | ?a :: nil = ?l1 ++ ?x :: ?l2 =>
@@ -97,6 +85,7 @@ Ltac unit_vs_elt_inv H :=
         | destruct l1 ; inversion H2 ]
   end.
 
+(* not for stdlib *)
 Lemma dichot_app {A} : forall (l1 : list A) l2 l3 l4,
   l1 ++ l2 = l3 ++ l4 ->
      (exists l2', l1 ++ l2' = l3 /\ l2 = l2' ++ l4)
@@ -126,7 +115,6 @@ induction l1 ; induction l3 ; intros ;
     simpl.
     rewrite H4'1...
 Qed.
-
 Ltac dichot_app_exec H :=
   match type of H with
   | _ ++ _ = _ ++ _ => apply dichot_app in H ;
@@ -137,6 +125,7 @@ Ltac dichot_app_exec H :=
                          destruct H as [(l2 & H1 & H2) | (l4 & H1 & H2)]
   end.
 
+(* not for stdlib *)
 Lemma dichot_elt_app {A} : forall l1 (a : A) l2 l3 l4,
   l1 ++ a :: l2 = l3 ++ l4 ->
      (exists l2', l1 ++ a :: l2' = l3 /\ l2 = l2' ++ l4)
@@ -162,7 +151,6 @@ induction l1 ; induction l3 ; intros ;
     simpl ;
     rewrite H'1...
 Qed.
-
 Ltac dichot_elt_app_exec H :=
   match type of H with
   | _ ++ _ :: _ = _ ++ _ => apply dichot_elt_app in H ;
@@ -180,6 +168,7 @@ Ltac dichot_elt_app_exec H :=
                               destruct H as [(l2 & H1 & H2) | (l4 & H1 & H2)]
   end.
 
+(* not for stdlib *)
 Lemma trichot_elt_app {A} : forall l1 (a : A) l2 l3 l4 l5,
   l1 ++ a :: l2 = l3 ++ l4 ++ l5 ->
       (exists l2', l1 ++ a :: l2' = l3 /\ l2 = l2' ++ l4 ++ l5)
@@ -204,7 +193,6 @@ induction l1 ; induction l3 ; intros ;
   + destruct H'3 ; subst ; exists l2' ; exists l2'' ; split ; [ | split ]...
   + exists l' ; try rewrite H'1 ; split...
 Qed.
-
 Ltac trichot_elt_app_exec H :=
   match type of H with
   | _ ++ _ :: _ = _ ++ _ ++ _ => apply trichot_elt_app in H ;
@@ -224,6 +212,7 @@ Ltac trichot_elt_app_exec H :=
                                                  | [ (l2 & l4 & H1 & H2) | (l4 & H1 & H2) ]]
   end.
 
+(* not for stdlib *)
 Lemma trichot_elt_elt {A} : forall l1 (a : A) l2 l3 b l4,
   l1 ++ a :: l2 = l3 ++ b :: l4 ->
       (exists l2', l1 ++ a :: l2' = l3 /\ l2 = l2' ++ b :: l4)
@@ -239,7 +228,6 @@ destruct l2' ; inversion H'2 ; subst ; [ | destruct l2' ; inversion H1 ].
 split ; [ | split ]...
 rewrite app_nil_r...
 Qed.
-
 Ltac trichot_elt_elt_exec H :=
   match type of H with
   | ?lh ++ _ :: ?lr = ?l1 ++ ?x :: ?l2 =>
@@ -253,6 +241,13 @@ Ltac trichot_elt_elt_exec H :=
         | (try subst x) ; (try subst l1) ; (try subst l2)
         | (try subst l2) ; (try subst lh) ]
   end.
+
+
+
+
+
+
+
 
 
 (** ** [In] *)
@@ -589,24 +584,6 @@ eapply Forall_forall ; try eassumption.
 apply in_elt.
 Qed.
 
-Lemma Forall_and_inv {A} : forall P Q (l : list A),
-  (Forall (fun x => P x /\ Q x) l) -> Forall P l /\ Forall Q l.
-Proof with try assumption.
-induction l ; intro Hl ; split ; constructor ; inversion Hl ; subst.
-- destruct H1...
-- apply IHl...
-- destruct H1...
-- apply IHl...
-Qed.
-
-Lemma Forall_and {A} : forall P Q (l : list A),
-  Forall P l -> Forall Q l -> Forall (fun x => P x /\ Q x) l.
-Proof.
-induction l; intros HP HQ; constructor; inversion HP; inversion HQ; subst.
-- now split.
-- now apply IHl.
-Qed.
-
 Lemma Forall_nth {A} : forall P l,
   Forall P l -> forall i (a : A), i < length l -> P (nth i l a).
 Proof with try assumption.
@@ -618,6 +595,7 @@ induction l ; intros.
   apply lt_S_n...
 Qed.
 
+(* not for stdlib ? *)
 Lemma exists_Forall {A B} : forall (P : A -> B -> Prop) l,
   (exists k, Forall (P k) l) -> Forall (fun x => exists k, P k x) l .
 Proof with try eassumption ; try reflexivity.
@@ -628,7 +606,7 @@ induction l ; intros ; constructor ;
   eexists...
 Qed.
 
-Lemma Forall_map {A B} : forall (f : A -> B) l,
+Lemma Forall_image {A B} : forall (f : A -> B) l,
   Forall (fun x => exists y, x = f y) l <-> exists l0, l = map f l0.
 Proof with try reflexivity.
 induction l ; split ; intro H.
@@ -664,6 +642,7 @@ induction l ; intros HP.
   constructor.
 Qed.
 
+(* not for stdlib ? *)
 Lemma inc_Forall {A} : forall (P : nat -> A -> Prop) l i j,
   (forall i j a, P i a -> i <= j -> P j a) ->
     Forall (P i) l -> i <= j -> Forall (P j) l.
@@ -672,6 +651,23 @@ intros P l i j Hinc.
 induction l ; intros H Hl ; constructor ; inversion H.
 - eapply Hinc...
 - apply IHl...
+Qed.
+
+Lemma Exists_app {A} : forall (P : A -> Prop) l1 l2,
+  Exists P l1 \/ Exists P l2 -> Exists P (l1 ++ l2).
+Proof with try assumption.
+induction l1 ; intros...
+- destruct H...
+  inversion H.
+- destruct H.
+  + inversion H ; subst.
+    * apply Exists_cons_hd...
+    * apply Exists_cons_tl.
+      apply IHl1.
+      left...
+  + apply Exists_cons_tl.
+    apply IHl1.
+    right...
 Qed.
 
 Lemma Exists_app_inv {A} : forall (P : A -> Prop) l1 l2,
@@ -689,23 +685,6 @@ induction l1 ; intros.
     * right...
 Qed.
 
-Lemma Exists_app {A} : forall (P : A -> Prop) l1 l2,
-  (Exists P l1 \/ Exists P l2) -> Exists P (l1 ++ l2).
-Proof with try assumption.
-induction l1 ; intros...
-- destruct H...
-  inversion H.
-- destruct H.
-  + inversion H ; subst.
-    * apply Exists_cons_hd...
-    * apply Exists_cons_tl.
-      apply IHl1.
-      left...
-  + apply Exists_cons_tl.
-    apply IHl1.
-    right...
-Qed.
-
 Lemma Exists_rev {A} : forall P (l : list A), Exists P l -> Exists P (rev l).
 Proof with try assumption.
 induction l ; intros HP ; inversion HP ; subst ;
@@ -713,31 +692,6 @@ induction l ; intros HP ; inversion HP ; subst ;
 - right ; constructor...
 - left.
   apply IHl...
-Qed.
-
-Lemma Exists_impl {A} : forall (P Q : A -> Prop), (forall a, P a -> Q a) ->
-  forall l, Exists P l -> Exists Q l.
-Proof.
-intros P Q Hi.
-induction l ; intros He ; inversion He ; subst.
-- apply Hi in H0 ; now constructor.
-- apply IHl in H0 ; now constructor.
-Qed.
-
-Lemma Exists_or_inv {A} : forall P Q (l : list A),
-  (Exists (fun x => P x \/ Q x) l) -> Exists P l \/ Exists Q l.
-Proof with try assumption.
-induction l ; intro Hl; inversion Hl; subst.
-- inversion H0; [left|right]; now constructor.
-- destruct (IHl H0) as [IHP|IHQ]; [left|right]; now constructor.
-Qed.
-
-Lemma Exists_or {A} : forall P Q (l : list A),
-  Exists P l \/ Exists Q l -> Exists (fun x => P x \/ Q x) l.
-Proof.
-induction l; intros [H|H]; inversion H; subst; try now repeat constructor.
-- eapply or_introl in H1; apply IHl in H1; now constructor.
-- eapply or_intror in H1; apply IHl in H1; now constructor.
 Qed.
 
 
