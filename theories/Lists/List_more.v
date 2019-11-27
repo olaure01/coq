90c2d157ef0365b81ef6dcbcc3497aa20674f78d
Olivier Laurent <olivier.laurent@ens-lyon.fr>
2019-11-27 10:25:24 +0100

    integration of statements for nth (remove from List_more)

--- theories/Lists/List_more.v
+++ theories/Lists/List_more.v
@@ -421,59 +421,9 @@ induction l1 ; intros.
 Qed.
 
 
-(* TODO HERE *)
-
 (* Properties on nth *)
-Lemma nth_nth {A} : forall (l1 : list nat) (l2 : list A) a0 k0 k,
-    k < length l1 ->
-    nth (nth k l1 k0) l2 a0 = nth k (map (fun x => nth x l2 a0) l1) a0.
-Proof with try assumption; try reflexivity.
-  induction l1; intros l2 a0 k0 k Hlt.
-  - inversion Hlt.
-  - destruct k...
-    simpl.
-    apply IHl1.
-    simpl in Hlt.
-    apply lt_S_n; assumption.
-Qed.
 
-Lemma nth_plus {A} : forall (l1 : list A) l2 k0 k,
-    nth (length l1 + k) (l1 ++ l2) k0 = nth k l2 k0.
-Proof with try reflexivity; try assumption.
-  induction l1; intros k2 k0 k...
-  simpl.
-  apply IHl1...
-Qed.
-
-Lemma nth_middle {A} : forall la lb (a : A) a0,
-    nth (length la) (la ++ a :: lb) a0 = a.
-Proof with try reflexivity.
-  induction la; intros lb a' a0...
-  simpl.
-  apply IHla.
-Qed.
-
-Lemma list_ext {A} : forall l1 l2,
-    length l1 = length l2 ->
-    (forall n (a0 : A), nth n l1 a0 = nth n l2 a0) ->
-    l1 = l2.
-Proof with try reflexivity.
-  induction l1; intros l2 Hlen H.
-  - destruct l2; try now inversion Hlen...
-  - destruct l2; try now inversion Hlen.
-    replace a0 with a.
-    2:{ change a with (nth 0 (a :: l1) a).
-        change a0 with (nth 0 (a0 :: l2) a).
-        apply H. }
-    apply Nat.succ_inj in Hlen.
-    specialize (IHl1 l2 Hlen).
-    clear Hlen.
-    replace l2 with l1...
-    apply IHl1.
-    intros n a1.
-    refine (H (S n) a1).
-Qed.
-
+(* not for stdlib (Type) *)
 Lemma nth_split_Type {A} n l (d:A) : n < length l ->
     {' (l1,l2) : _ & l = l1 ++ nth n l d :: l2 & length l1 = n }.
   Proof.
